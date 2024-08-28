f03a69fcd5076a7f4f28174e6dfa41430d144e9f
Yixiao Sun <129694510+calebsun241@users.noreply.github.com>
2024-08-28 16:44:45 -0400

    Add files via upload

---
+++ src/assessts/index.js
@@ -0,0 +1,134 @@
+import backend from "./backend.png";
+import close from "./close.svg";
+import fullstack from "./creator.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+import aws from "./tech/aws.png";
+import cplusplus from "./tech/c++.svg";
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import java from "./tech/java.png";
+import javascript from "./tech/javascript.png";
+import linux from "./tech/linux.png";
+import mysql from "./tech/mysql.png";
+import node_js from "./tech/node_js.svg";
+import python from "./tech/python.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import ubuntu from "./tech/ubuntu.png";
+
+import cisco from "./tech/cisco.svg";
+import connectwise from "./tech/connectwise.png";
+import powershell from "./tech/powershell.svg";
+import virtualbox from "./tech/virtualbox.png";
+
+import aircrackng from "./tech/aircrackng.svg";
+import hydra from "./tech/hydra.svg";
+import johntheripper from "./tech/johntheripper.svg";
+import kalilinux from "./tech/kali_linux.svg";
+import metasploit from "./tech/metasploit.svg";
+import nmap from "./tech/nmap.svg";
+import wireshark from "./tech/wireshark.png";
+
+import blender from "./tech/blender.svg";
+import cinema4d from "./tech/cinema4d.png";
+import photoshop from "./tech/photoshop.svg";
+import premiere from "./tech/premiere.svg";
+
+import oist from "./oist.png";
+import samaritans from "./samaritans.png";
+
+//
+import freecodecamp from "./company/freecodecamp.png";
+import google from "./company/google.png";
+import mackenziehealth from "./company/mackenziehealth.png";
+import otu from "./company/otu.png";
+import privcurity from "./company/privcurity.jpg";
+import rhhs from "./company/rhhs.jpg";
+import whmis from "./company/whmis.png";
+import wonderland from "./company/wonderland.jpg";
+
+import password_generator from "./passwordgenerator.png";
+import wordsearch from "./wordsearchoutput.jpg";
+//
+
+import cognizant from "./company/cognizant.png";
+import internshala from "./company/internshala.webp";
+import nextinnovation from "./company/nextinnovation.jpeg";
+
+import hackerrank from "./company/hackerrank.png";
+import linkedin from "./company/linkedin.png";
+
+import portfolio from "./3dportfolio.png";
+import pglife from "./pglife.png";
+import sortingvisualiser from "./sortingvisualiser.png";
+
+import connectwisecert from "./Connectwise-Scripting-Certification.pdf";
+import resume from "./Yixiao-Sun-Resume.pdf";
+
+export {
+  aircrackng,
+  aws,
+  backend,
+  blender,
+  cinema4d,
+  cisco,
+  close,
+  cognizant,
+  connectwise,
+  connectwisecert,
+  cplusplus,
+  css,
+  freecodecamp,
+  fullstack,
+  git,
+  github,
+  google,
+  hackerrank,
+  html,
+  hydra,
+  internshala,
+  java,
+  javascript,
+  johntheripper,
+  kalilinux,
+  linkedin,
+  linux,
+  mackenziehealth,
+  menu,
+  metasploit,
+  mobile,
+  mysql,
+  nextinnovation,
+  nmap,
+  node_js,
+  oist,
+  otu,
+  password_generator,
+  pglife,
+  photoshop,
+  portfolio,
+  powershell,
+  premiere,
+  privcurity,
+  python,
+  reactjs,
+  resume,
+  rhhs,
+  samaritans,
+  sortingvisualiser,
+  tailwind,
+  typescript,
+  ubuntu,
+  virtualbox,
+  web,
+  whmis,
+  wireshark,
+  wonderland,
+  wordsearch,
+};
